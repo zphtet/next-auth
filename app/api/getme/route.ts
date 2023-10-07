4fa4e9f331e8e49e1ac1a8f1142a70078a90ec4a
zphtet <zinphtet.215108@gmail.com>
2023-10-07 19:52:08 +0630

    update url

--- app/api/getme/route.ts
+++ app/api/getme/route.ts
@@ -1,13 +1,7 @@
-import authOption from "@/lib/authOptions";
-import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/connectDB";
-
 export async function GET(request: Request) {
-  const session = await getServerSession(authOption);
-  await connectDB();
-  console.log(session);
   return NextResponse.json({
-    session,
+    code: process.env.RESEND_CODE,
+    url: process.env.LIVE_URL,
   });
 }
--- app/api/sendemail/route.tsx
+++ app/api/sendemail/route.tsx
@@ -3,12 +3,16 @@ import Confirm from "../../../emails/Confirm";
 import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const resend = new Resend(process.env.RESEND_CODE);
+
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: "zinpainghtet.215108@gmail.com",
     subject: "verify your email address",
     react: (
-      <Confirm baseUrl={process.env.LIVE_URL!} token="token-from-api-route" />
+      <Confirm
+        baseUrl={process.env.LIVE_URL! || "http://localhost:3000"}
+        token="token-from-api-route"
+      />
     ),
   });
 
